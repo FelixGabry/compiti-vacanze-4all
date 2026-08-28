b19d69f9566ac255066c427bd490d2a712e583af
FelixGabry <211388214+FelixGabry@users.noreply.github.com>
2026-08-28 17:15:51 +0200

    Mark Pirandello as done with four printable study sheets.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "compiti-4all-v13";
+const CACHE = "compiti-4all-v14";
 const FILES = ["./index.html", "./styles.css", "./app.js", "./data.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
