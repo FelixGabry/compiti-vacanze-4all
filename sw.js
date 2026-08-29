cc5341fa51d529fd29107694aca0e13228eaedaf
FelixGabry <211388214+FelixGabry@users.noreply.github.com>
2026-08-29 02:25:51 +0200

    Mark A Christmas Carol as done with six printable study sheets.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "compiti-4all-v14";
+const CACHE = "compiti-4all-v15";
 const FILES = ["./index.html", "./styles.css", "./app.js", "./data.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
